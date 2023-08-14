2836d9c94f9646f3a95451d7201f32c4b47003e7
Senzaj <sanamistik@mail.ru>
2023-08-14 22:37:39 +0300

    Added UI upgrades

--- Assets/Sources/Modules/Player/MVP/PlayerView.cs
+++ Assets/Sources/Modules/Player/MVP/PlayerView.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace Sources.Modules.Player.MVP
 {
@@ -9,19 +10,31 @@ namespace Sources.Modules.Player.MVP
         [SerializeField] private Button _maxHealthButton;
         [SerializeField] private Button _addDamageButton;
         [SerializeField] private Button _addSpeedButton;
-        [SerializeField] private Text _healthText;
-        [SerializeField] private Text _damageText;
-        [SerializeField] private Text _speedText;
+        [SerializeField] private TMP_Text _currentHealthText;
+        [SerializeField] private TMP_Text _upgradeHealthText;
+        [SerializeField] private TMP_Text _currentDamageText;
+        [SerializeField] private TMP_Text _upgradeDamageText;
+        [SerializeField] private TMP_Text _currentSpeedText;
+        [SerializeField] private TMP_Text _upgradeSpeedText;
         
         public event Action ButtonAddMaxHealthPressed;
         public event Action ButtonAddDamageScalerPressed;
         public event Action ButtonAddSpeedPressed;
 
+        private PlayerModel _playerModel;
+        
         private void Awake()
         {
             _maxHealthButton.onClick.AddListener((() => ButtonAddMaxHealthPressed?.Invoke()));
             _addDamageButton.onClick.AddListener((() => ButtonAddDamageScalerPressed?.Invoke()));
             _addSpeedButton.onClick.AddListener((() => ButtonAddSpeedPressed?.Invoke()));
+        }
+
+        private void OnEnable()
+        {
+            ChangeMaxHealthText(_playerModel.MaxHealth);
+            ChangeDamageScalerText(_playerModel.DamageScaler);
+            ChangeSpeedText(_playerModel.Speed);
         }
 
         private void OnDisable()
@@ -31,19 +44,30 @@ namespace Sources.Modules.Player.MVP
             _addSpeedButton.onClick.RemoveListener((() => ButtonAddSpeedPressed?.Invoke()));
         }
 
+        public void SetPlayerModel(PlayerModel model)
+        {
+            _playerModel = model;
+        }
+        
         public void ChangeMaxHealthText(float maxHealth)
         {
-            
+            _currentHealthText.text = Mathf.CeilToInt(maxHealth).ToString();
+            _upgradeHealthText.text =
+                Mathf.CeilToInt(_playerModel.MaxHealthIncreaseValue + maxHealth).ToString();
         }
         
         public void ChangeDamageScalerText(float damageScaler)
         {
-            
+            _currentDamageText.text = Mathf.CeilToInt(damageScaler).ToString();
+            _upgradeDamageText.text =
+                Mathf.CeilToInt(damageScaler + _playerModel.DamageScalerIncreaseValue).ToString();
         }
         
         public void ChangeSpeedText(float speed)
         {
-            
+            _currentSpeedText.text = Mathf.CeilToInt(speed).ToString();
+            _upgradeSpeedText.text =
+                Mathf.CeilToInt(speed + _playerModel.SpeedIncreaseValue).ToString();
         }
     }
 }
