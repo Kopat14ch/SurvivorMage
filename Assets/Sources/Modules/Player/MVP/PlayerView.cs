fcc818b80c7ba68e9070821008ca2696ad239c5a
Senzaj <sanamistik@mail.ru>
2023-08-14 20:29:01 +0300

    Features

--- Assets/Sources/Modules/Player/MVP/PlayerView.cs
+++ Assets/Sources/Modules/Player/MVP/PlayerView.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Sources.Modules.Player.MVP
 {
     public class PlayerView : MonoBehaviour
     {
+        [SerializeField] private Button _maxHealthButton;
+        [SerializeField] private Button _addDamageButton;
+        [SerializeField] private Button _addSpeedButton;
+        [SerializeField] private Text _healthText;
+        [SerializeField] private Text _damageText;
+        [SerializeField] private Text _speedText;
+        
         public event Action ButtonAddMaxHealthPressed;
         public event Action ButtonAddDamageScalerPressed;
         public event Action ButtonAddSpeedPressed;
+
+        private void Awake()
+        {
+            _maxHealthButton.onClick.AddListener((() => ButtonAddMaxHealthPressed?.Invoke()));
+            _addDamageButton.onClick.AddListener((() => ButtonAddDamageScalerPressed?.Invoke()));
+            _addSpeedButton.onClick.AddListener((() => ButtonAddSpeedPressed?.Invoke()));
+        }
+
+        private void OnDisable()
+        {
+            _maxHealthButton.onClick.RemoveListener((() => ButtonAddMaxHealthPressed?.Invoke()));
+            _addDamageButton.onClick.RemoveListener((() => ButtonAddDamageScalerPressed?.Invoke()));
+            _addSpeedButton.onClick.RemoveListener((() => ButtonAddSpeedPressed?.Invoke()));
+        }
 
         public void ChangeMaxHealthText(float maxHealth)
         {
