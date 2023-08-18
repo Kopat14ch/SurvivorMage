9dd5fff722abde9bf67e7d8962c9b7c3666647f9
Senzaj <sanamistik@mail.ru>
2023-08-18 12:48:24 +0300

    Added ParticleSpawner and PartilcePool

---
+++ Assets/Sources/Modules/Particles/Scripts/ParticleType.cs
@@ -0,0 +1,8 @@
+namespace Sources.Modules.Particles.Scripts
+{
+    public enum ParticleType 
+    {
+        MageDamaged,
+        EnemyDied
+    }
+}
---
+++ Assets/Sources/Modules/Particles/Scripts/Pool/ParticlePool.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sources.Modules.Particles.Scripts.Pool
+{
+    public class ParticlePool : MonoBehaviour
+    {
+        [SerializeField] private List<Particle> _prefabs;
+        [SerializeField] private int _startCapacity;
+        [SerializeField] private ParticleContainer _prefabContainer;
+
+        private List<ParticleContainer> _containers;
+
+        public void Init()
+        {
+            _containers = new List<ParticleContainer>();
+
+            foreach (Particle prefab in _prefabs)
+            {
+                ParticleContainer container = Instantiate(_prefabContainer, transform.position, Quaternion.identity,
+                    transform);
+                container.Init(prefab.ParticleType, prefab);
+                _containers.Add(container);
+                
+                for (int i = 0; i < _startCapacity; i++)
+                {
+                    Particle spawned = Instantiate(prefab, transform.position, Quaternion.identity, container.transform);
+                    spawned.Disable();
+                    container.AddParticle(spawned);
+                }
+            }
+        }
+
+        public Particle GetObject(ParticleType particleType)
+        {
+            Particle particle = null;
+
+            foreach (ParticleContainer container in _containers)
+            {
+                if (container.ParticleType == particleType)
+                {
+                    particle = container.GetParticle();
+                    break;
+                }
+            }
+
+            return particle;
+        }
+    }
+}
