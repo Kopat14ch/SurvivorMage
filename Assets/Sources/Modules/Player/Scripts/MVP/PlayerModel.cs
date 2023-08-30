c6ddc592ee127c9468ce614f7fabd6ecc09cfacf
Senzaj <sanamistik@mail.ru>
2023-08-31 01:57:56 +0300

    Added UIAudio buttons and a bit rebalanced

--- Assets/Sources/Modules/Player/Scripts/MVP/PlayerModel.cs
+++ Assets/Sources/Modules/Player/Scripts/MVP/PlayerModel.cs
@@ -4,7 +4,7 @@ namespace Sources.Modules.Player.Scripts.MVP
 {
     internal class PlayerModel
     {
-        private const float MaxHealthIncreaseValue = 5f;
+        private const float MaxHealthIncreaseValue = 10f;
         private const float DamageScalerIncreaseValue = 0.1f;
         private const float SpeedIncreaseValue = 0.1f;
         private readonly PlayerData _playerData;
--- Assets/Sources/Modules/UI/Scripts/PausePanel/MusicVolumeSlider.cs
+++ Assets/Sources/Modules/UI/Scripts/PausePanel/MusicVolumeSlider.cs
@@ -9,20 +9,49 @@ namespace Sources.Modules.UI.Scripts.PausePanel
     {
         [SerializeField] private BackgroundSound _backgroundSound;
         [SerializeField] private Slider _slider;
+        [SerializeField] private Button _volumeButton;
+        [SerializeField] private CanvasGroup _redLine;
+
+        private const float MinVolume = 0;
+        private const float MaxVolume = 1;
+        private const float AlphaRedLineDisabled = 0;
+        private const float AlphaRedLineEnabled = 1;
 
         private void OnEnable()
         {
             _slider.onValueChanged.AddListener(ChangeVolume);
+            _volumeButton.onClick.AddListener(OnButtonPressed);
+            TryChangeButtonView();
         }
-        
+
         private void OnDisable()
         {
             _slider.onValueChanged.RemoveListener(ChangeVolume);
+            _volumeButton.onClick.RemoveListener(OnButtonPressed);
         }
 
+        private void OnButtonPressed()
+        {
+            if (_slider.value == MinVolume)
+                SetVolumeByButton(MaxVolume, AlphaRedLineDisabled);
+            else
+                SetVolumeByButton(MinVolume, AlphaRedLineEnabled);
+        }
+
+        private void SetVolumeByButton(float volume, float alphaRedLine)
+        {
+            _slider.value = volume;
+            ChangeVolume(volume);
+            _redLine.alpha = alphaRedLine;
+        }
+        
+        private void TryChangeButtonView() =>
+            _redLine.alpha = _slider.value == MinVolume ? AlphaRedLineEnabled : AlphaRedLineDisabled;
+        
         private void ChangeVolume(float volume)
         {
             _backgroundSound.ChangeVolume(volume);
+            TryChangeButtonView();
         }
     }
 }
